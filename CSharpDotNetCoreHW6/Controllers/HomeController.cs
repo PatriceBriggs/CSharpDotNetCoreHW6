f0988d09349502622185d660a2247ad4c4796e4e
Patrice Briggs <PBriggs@perkinscoie.com>
2021-06-10 16:45:21 -0700

    Initial Commit Homework 6

---
+++ CSharpDotNetCoreHW6/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using CSharpDotNetCoreHW6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CSharpDotNetCoreHW6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new BirthdayCardModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Models.BirthdayCardModel birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("ThankYou", birthdayCard);
+            }
+            else
+            {
+                return View(birthdayCard);
+            }
+        }
+    }
+}
---
+++ CSharpDotNetCoreHW6/Models/BirthdayCardModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpDotNetCoreHW6.Models
+{
+    public class BirthdayCardModel
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string  From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+    }
+}
